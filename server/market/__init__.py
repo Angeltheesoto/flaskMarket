68c2751998dba2551edcbeec76d336d9630997ea
Your Name01~ <you@example.com>
2023-04-02 18:08:27 -0400

    Finished User Authentication

--- server/market/__init__.py
+++ server/market/__init__.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template
 from flask_cors import CORS
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = 'ed20cd685c789acaea6fdb10'
 db = SQLAlchemy(app)
-CORS(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 # routes
 from market import routes
